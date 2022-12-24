44299d593e8915c2b383af58f05ec41b85c6568f
Andoni Romano <Andoni Romano>
2022-12-23 18:57:19 -0600

    display info

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,32 +1,39 @@
-import { info } from "autoprefixer";
 import React, { useState, useEffect } from "react";
-const mainInfo = [];
+import axios from "axios";
 
 const Popular = () => {
-  async function fetchRecommended() {
-    await fetch(
-      `https://api.themoviedb.org/3/movie/2/recommendations?api_key=d15db2786c7e9576fe22f4a84bea426e`
-    )
-      .then((res) => res.json())
-      .then((info) => {
-        let information = info?.results;
-        Promise.all(information).then((res) => {
-          mainInfo.push(res);
-        });
-      });
+  let infoContainer = [];
+  const [movies, setMovies] = useState([]);
 
-    return mainInfo;
+  function fetchData() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/2/recommendations?api_key=d15db2786c7e9576fe22f4a84bea426e`
+      )
+      .then((response) => {
+        console.log(response.data.results);
+        setMovies(response.data.results);
+      })
+      .catch((err) => console.log(err));
   }
 
   useEffect(() => {
-    fetchRecommended();
-    console.log(mainInfo);
+    fetchData();
   }, []);
 
   return (
     <div>
       <div className="mt-4 ml-44 text-3xl">Recommended Movies</div>
-      <div></div>
+      <div className="bg-blue-300">
+        {movies.map((res, index) => {
+          return (
+            <div key={index}>
+              <img src={res.poster_path} />
+              <div>{res.title}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
